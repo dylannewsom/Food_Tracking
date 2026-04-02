a7d196cf569e272c935029fbf9fb1a3cc0e0a159
dylannewsom <dylannewsom@gmail.com>
2026-04-02 17:28:13 -0500

    initial commit

---
+++ prisma.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  schema: "prisma/schema.prisma",
+  migrations: {
+    path: "prisma/migrations",
+  },
+  datasource: {
+    url: process.env.DATABASE_URL,
+    directUrl: process.env.DIRECT_URL,
+  },
+};
